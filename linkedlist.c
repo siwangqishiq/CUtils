36530255ae6b9b37d156852f2980ddec9a317d5a
panyi <525647740@qq.com>
2017-01-04 17:26:49 +0800

    双向链表插入操作

--- linkedlist.c
+++ linkedlist.c
@@ -15,21 +15,78 @@ Linkedlist *linkedlist_create(){
 
 void linkedlist_free(Linkedlist *list){
     if(list){
+        List_Node *p = list->head;
+        while(p){
+            List_Node *t_prior = p;
+            p = p->next;
+            free(t_prior);
+        }//end while
         
+        free(list);
     }
 }
 
 int linkedlist_add(Linkedlist *list,Type add_data){
-    if(!list)
+    return linkedlist_add_at_index(list,list->size,add_data);
+}
+
+int linkedlist_add_at_index(Linkedlist *list,int index,Type add_data){
+    if(!list || index < 0 || !add_data)
         return ERROR;
     
+    List_Node *node = linkedlist_create_list_node(add_data);
+    if(!node)
+        return ERROR;
+    
+    if(index == 0 || list->size == 0){//insert to the head
+        List_Node *p = list->head;
+        list->head = node;
+        list->head->next = p;
+        list->head->prior = NULL;
+        
+        if(p)
+            p->prior = list->head;
+    }else{//insert to the other
+        if(index >= list->size)
+            index = list->size;
+        
+        List_Node *p = list->head;
+        List_Node *prior_node = NULL;
+        int count = 0;
+        while(count < index && p){
+            count++;
+            
+            if(!p->next)
+                prior_node = p;
+             p = p->next;
+        }//end while
+        
+        if(p)
+            prior_node = p->prior;
+        
+        List_Node *next_node = p;
+        
+        if(prior_node){
+            prior_node->next = node;
+            node->prior = prior_node;
+        }
+        
+        if(next_node){
+            node->next = next_node;
+            next_node->prior = node;
+        }
+        
+    }//end if
     
     list->size++;
     
-    return ERROR;
+    return OK;
 }
 
 int linkedlist_remove_at_index(Linkedlist *list,int index){
+    if(!list || index < 0 || index >= list->size)
+        return ERROR;
+    
     return ERROR;
 }
 
@@ -38,5 +95,27 @@ int linkedlist_remove_element(Linkedlist *list,Type remove_item){
 }
 
 Type linkedlist_get_element(Linkedlist *list,int index){
-    return NULL;
+    if(!list || index < 0)
+        return NULL;
+    
+    List_Node *p = list->head;
+    int count = 0;
+    while(p && count < index){
+        p = p->next;
+        count++;
+    }//end while
+    
+    return p?p->data:NULL;
 }
+
+//----- private function--------
+List_Node *linkedlist_create_list_node(Type data){
+    List_Node *node = (List_Node *)malloc(sizeof(List_Node));
+    if(!node)
+        return NULL;
+    memset(node, 0, sizeof(List_Node));
+    node->data = data;
+    
+    return node;
+}
+
--- linkedlist.h
+++ linkedlist.h
@@ -11,16 +11,19 @@
 
 typedef void* Type;
 
-typedef struct _List_Node{
+struct _List_Node{
     Type *data;
-    struct List_Node *next;
-    struct List_Node *prior;
-} List_Node;
+    struct _List_Node *next;
+    struct _List_Node *prior;
+};
+
+typedef struct _List_Node List_Node;
 
 struct _Linkedlist{
     int size;
     List_Node *head;
 };
+
 typedef struct _Linkedlist Linkedlist;
 
 Linkedlist *linkedlist_create();
@@ -29,10 +32,14 @@ void linkedlist_free(Linkedlist *list);
 
 int linkedlist_add(Linkedlist *list,Type add_data);
 
+int linkedlist_add_at_index(Linkedlist *list,int index,Type add_data);
+
 int linkedlist_remove_at_index(Linkedlist *list,int index);
 
 int linkedlist_remove_element(Linkedlist *list,Type remove_item);
 
 Type linkedlist_get_element(Linkedlist *list,int index);
+
+List_Node *linkedlist_create_list_node(Type data);
 
 #endif
